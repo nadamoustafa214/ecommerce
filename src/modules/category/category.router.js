97b202fa385f24744d6588ffcafce5e98f20f5aa
nada moustafa <148712191+nadamoustafa214@users.noreply.github.com>
2024-10-07 15:13:43 +0300

    cahanging for dep;oy

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -15,7 +15,7 @@ router.use('/:categoryId/subCategory',subCategory)
 router.post('/newCategory',auth(endPoint.create),fileUpload(fileValidation.image).single('image'),validation(validators.newCategorySchema),CC.createCategory)
 router.put('/updateCategory/:categoryId',fileUpload(fileValidation.image).single('image'),validation(validators.updateCategorySchema),asyncHandler(CC.updateCateory))
 router.patch('/deleteCategory/:categoryId',validation(validators.deleteCategorySchema),asyncHandler(CC.deleteCategory))
-router.get('/allCategories',auth(Object.values(roles)),asyncHandler(CC.getAllCatigories))
+router.get('/allCategories',asyncHandler(CC.getAllCatigories))
 router.post('/:categoryId',validation(validators.getOneCategorySchema),asyncHandler(CC.getOneCategory))
 
 
